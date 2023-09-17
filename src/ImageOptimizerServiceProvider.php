a2fc1246b31f97d95828adc73ae8a21e0ad1f613
Josh Embling <joshembling@gmail.com>
2023-09-17 13:29:40 +0100

    Update provider, rm unused files

--- src/ImageOptimizerServiceProvider.php
+++ src/ImageOptimizerServiceProvider.php
@@ -2,14 +2,7 @@
 
 namespace Joshembling\ImageOptimizer;
 
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
-use Joshembling\ImageOptimizer\Commands\ImageOptimizerCommand;
 use Joshembling\ImageOptimizer\Testing\TestsImageOptimizer;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
@@ -20,17 +13,9 @@ class ImageOptimizerServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-image-optimizer';
 
-    public static string $viewNamespace = 'filament-image-optimizer';
-
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package->name(static::$name)
-            ->hasCommands($this->getCommands())
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
@@ -45,39 +30,13 @@ class ImageOptimizerServiceProvider extends PackageServiceProvider
             $package->hasConfigFile();
         }
 
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
         if (file_exists($package->basePath('/../resources/lang'))) {
             $package->hasTranslations();
         }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
-        }
-    }
-
-    public function packageRegistered(): void
-    {
     }
 
     public function packageBooted(): void
     {
-        // Asset Registration
-        FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
-        );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
         // Handle Stubs
         if (app()->runningInConsole()) {
             foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
@@ -89,66 +48,5 @@ class ImageOptimizerServiceProvider extends PackageServiceProvider
 
         // Testing
         Testable::mixin(new TestsImageOptimizer());
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'joshembling/filament-image-optimizer';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('filament-image-optimizer', __DIR__ . '/../resources/dist/components/filament-image-optimizer.js'),
-            Css::make('filament-image-optimizer-styles', __DIR__ . '/../resources/dist/filament-image-optimizer.css'),
-            Js::make('filament-image-optimizer-scripts', __DIR__ . '/../resources/dist/filament-image-optimizer.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            ImageOptimizerCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_filament-image-optimizer_table',
-        ];
     }
 }
